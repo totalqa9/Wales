b89a140f1b3729a6161cd0fb81ac1cafa08dcb2c
sudheer51 <sudheer694@gmail.com>
2020-05-23 07:24:01 +0530

    Removing the commented code

--- mmp/src/test/java/SampleTestngProgram.java
+++ mmp/src/test/java/SampleTestngProgram.java
@@ -3,19 +3,6 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SampleTestngProgram {
-
-	
-//	@BeforeTest
-//	public void m1()
-//	{
-//		String s = null;
-//		System.out.println(s.length());
-//	}
-//	@BeforeClass(alwaysRun=true)
-//	public void display()
-//	{
-//		System.out.println("in display");
-//	}
 	
 	@Test
 	public  void m2()
